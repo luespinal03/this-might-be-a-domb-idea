bfc99f5afd00699f5fbf78c4cef934c39a000032
Luis Espinal <luise@MacBook-Pro.attlocal.net>
2022-06-28 14:11:09 -0400

    main.js

---
+++ main.js
@@ -0,0 +1,34 @@
+let para = document.querySelector('p');
+
+para.style.color = 'lightblue'
+para.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
+
+let h = document.querySelector('h1');
+
+h.style.fontSize = "48px"
+
+let unotres = document.querySelector('#item-13');
+
+// unotres.style.color = '#f0f0f0';
+unotres.style.opacity ='0.5';
+let turd = document.querySelector('#item-3')
+
+turd.innerText = 'Hi!'
+
+let image = document.querySelector("img");
+
+image.src = "http://www.tioxic.com/wp-content/uploads/trex_4.jpg"
+
+image.style.height = '300px'
+
+let myImage = document.querySelector("#assasins");
+
+myImage.src = "https://i.redd.it/g7amgob7dsr41.jpg"
+
+myImage.style.height = '300px'
+
+let mySixTeen = document.querySelector('#item-16');
+
+mySixTeen.className = 'item';
+
+mySixTeen.innerText = "Won't get me fooled again"
